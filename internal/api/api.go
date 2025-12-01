c7c29fa3207250e571c761315cfaa53a9abde1a5
NicoBernardes <nicolasbernardes84@gmail.com>
2025-12-01 17:38:21 -0300

    added sessions/cookies config

--- internal/api/api.go
+++ internal/api/api.go
@@ -4,12 +4,14 @@ import (
 	"net/http"
 
 	"github.com/NicoBernardes/auction-house-go.git/internal/services"
+	"github.com/alexedwards/scs/v2"
 	"github.com/go-chi/chi/v5"
 )
 
 type Api struct {
 	Router      *chi.Mux
 	UserService services.UserService
+	Sessions    *scs.SessionManager
 }
 
 func (api *Api) handleCreateUser(w http.ResponseWriter, r *http.Request) {}
--- internal/services/users_service.go
+++ internal/services/users_service.go
@@ -6,12 +6,16 @@ import (
 
 	"github.com/NicoBernardes/auction-house-go.git/internal/store/pgstore"
 	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgconn"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"golang.org/x/crypto/bcrypt"
 )
 
-var ErrDuplicatedEmailOrUsername = errors.New("username or email already exists")
+var (
+	ErrDuplicatedEmailOrUsername = errors.New("username or email already exists")
+	ErrInvallidCredentials       = errors.New("invalid credentials")
+)
 
 type UserService struct {
 	pool    *pgxpool.Pool
@@ -46,4 +50,22 @@ func (us *UserService) CreateUser(ctx context.Context, userName, email, password
 		return uuid.UUID{}, err
 	}
 	return id, nil
+}
+
+func (us *UserService) AuthenticateUser(ctx context.Context, email, password string) (uuid.UUID, error) {
+	user, err := us.queries.GetUserByEmail(ctx, email)
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return uuid.UUID{}, ErrInvallidCredentials
+		}
+		return uuid.UUID{}, err
+	}
+	err = bcrypt.CompareHashAndPassword(user.PasswordHash, []byte(password))
+	if err != nil {
+		if errors.Is(err, bcrypt.ErrMismatchedHashAndPassword) {
+			return uuid.UUID{}, ErrInvallidCredentials
+		}
+		return uuid.UUID{}, err
+	}
+	return user.ID, nil
 }
